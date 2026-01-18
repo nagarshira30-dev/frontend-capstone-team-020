148efeb8481f10eeae10656b15740c3f82e0d585
nagarshira30-tech <nagarshira30@gmail.com>
2026-01-18 21:52:10 +0200

    Changing the top

--- j4.js
+++ j4.js
@@ -6,8 +6,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const searchToggle = document.getElementById('search-toggle');
+    const searchMobileToggle = document.getElementById('search-toggle-mobile');
     const searchbar = document.getElementById('searchbar');
     const searchClose = document.getElementById('search-close');
+
+
+    const caretMobileToggle = document.getElementById('caret-mobile');
+    const mobileMenu = document.querySelector('.mobile-menu-container')
+    const darkMode = document.getElementById('dark-mode');
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+    let darkModeOn = false;
+    let caretMobileMenuOn = false;
+
+    // פונקציית מעבר בין מצב כהה למצב בהיר
+    darkModeToggle.addEventListener('click', () => {
+        if (!darkModeOn) {
+            darkMode.className = "fa-regular fa-moon";
+            darkModeOn = true;
+        } else {
+            darkMode.className = "fa-solid fa-moon";
+            darkModeOn = false;
+        }
+    })
+
+    // פונקציית תפריט מצב טלפון
+    caretMobileToggle.addEventListener('click', (event) => {
+        event.stopPropagation();
+        if (!caretMobileMenuOn) {
+            mobileMenu.style.display = 'block';
+            caretMobileMenuOn = true;
+        } else {
+            mobileMenu.style.display = 'none';
+            caretMobileMenuOn = false;
+        }
+    })
 
 
     // פונקציות תפריט צד
@@ -17,6 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // פונקציות חיפוש
     searchToggle.addEventListener('click', () => searchbar.classList.add('is-open'));
+    searchMobileToggle.addEventListener('click', () => searchbar.classList.add('is-open'));
     searchClose.addEventListener('click', () => searchbar.classList.remove('is-open'));
 
 
@@ -24,6 +58,11 @@ document.addEventListener('DOMContentLoaded', () => {
     sidebar.addEventListener('click', (e) => {
         if (e.target === sidebar) sidebar.classList.remove('is-open');
     });
+
+    document.body.addEventListener('click', () => {
+        mobileMenu.style.display = 'none';
+        caretMobileMenuOn = false;
+    })
 });
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('loginBtn');
